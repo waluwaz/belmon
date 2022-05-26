7acaaed251ac93d6170731904df2e5217411e7e3
waluwaz <github.282412@antichef.com>
2022-05-26 15:21:14 +0200

    Executed every hour (instead of 4 times an hour), at H:46.

--- belmon.sh
+++ belmon.sh
@@ -444,7 +444,7 @@ Auto_Cron(){
 			STARTUPLINECOUNT=$(cru l | grep -c "$SCRIPT_NAME")
 			
 			if [ "$STARTUPLINECOUNT" -eq 0 ]; then
-				cru a "$SCRIPT_NAME" "1,16,31,46 * * * *  /jffs/scripts/$SCRIPT_NAME generate"
+				cru a "$SCRIPT_NAME" "46 * * * *  /jffs/scripts/$SCRIPT_NAME generate"
 			fi
 		;;
 		delete)
